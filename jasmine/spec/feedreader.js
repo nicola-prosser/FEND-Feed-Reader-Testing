53ad2125d813c046bd9971a861dca2cf9e314759
Nicola-Prosser <nicola.prosser@outlook.com>
2018-06-23 20:06:43 +0100

    required tests added to jasmine specs

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -91,30 +91,53 @@ $(function() {
     /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function(){
 
-      /* TODO: Write a test that ensures when the loadFeed
-       * function is called and completes its work, there is at least
-       * a single .entry element within the .feed container.
-       * Remember, loadFeed() is asynchronous so this test will require
-       * the use of Jasmine's beforeEach and asynchronous done() function.
-       */
+                /* TODO: Write a test that ensures when the loadFeed
+                * function is called and completes its work, there is at least
+                * a single .entry element within the .feed container.
+                * Remember, loadFeed() is asynchronous so this test will require
+                * the use of Jasmine's beforeEach and asynchronous done() function.
+                */
 
-      it('successfully calls loadFeed and there is at least a single entry', function() {
+                beforeEach(function (done) {
+                  loadFeed(0, function (){
+                    done();
+                  });
+                });
 
-        //create loop to check each entry
-        
-      });
+                it('successfully calls loadFeed and there is at least a single entry', function() {
+                  const feedTotal = document.querySelector('.feed').getElementsByClassName('entry').length;
+                  expect(feedTotal).not.toBe(0);
+                });
     });
 
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-    describe('New Feed Selector', function(){
+    describe ('New Feed Selection', function() {
 
-      /* TODO: Write a test that ensures when a new feed is loaded
-       * by the loadFeed function that the content actually changes.
-       * Remember, loadFeed() is asynchronous.
-       */
+                /* TODO: Write a test that ensures when a new feed is loaded
+                * by the loadFeed function that the content actually changes.
+                * Remember, loadFeed() is asynchronous.
+                */
 
-      it('content actually changes when new feed is called', function() {});
+                //load first feed
+                let FeedSelection;
+
+                beforeEach(function(done) {
+                  loadFeed(0, function() {
+                    FeedSelection = document.querySelector(".feed").innerHTML;
+
+                    loadFeed(1, function() {
+                      done();
+                    });
+                  });
+                });
+
+        // when loadFeed calls new content - ensure it changes
+        it("loadFeed calls new content", function(done) {
+          var newFeedSelection = document.querySelector(".feed").innerHTML;
+          expect(FeedSelection).not.toBe(newFeedSelection);
+          done();
+        });
 
 
     });
